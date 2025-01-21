f07aa5f92fd93081dbfb07b49cf3c3b058b9d32c
JesicaC266 <jesicacasallas06@gmail.com>
2025-01-21 16:18:17 -0500

    ejercicio 6

--- 6-Piramide.py
+++ 6-Piramide.py
@@ -1,8 +1,8 @@
 filas = 7
 
 for i in range(1, filas + 1):
-    # Imprimir espacios antes de los asteriscos
+
     espacios = " " * (filas - i)
-    # Imprimir asteriscos
+    
     asteriscos = "*" * (2 * i - 1)
     print(espacios + asteriscos)
