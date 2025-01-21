80f995de1874ca424703dd3fec7fa9ffb0886a36
JesicaC266 <jesicacasallas06@gmail.com>
2025-01-21 11:32:04 -0500

    act bucles

--- 5-Bucles.py
+++ 5-Bucles.py
@@ -1,3 +1,15 @@
-personas = {'Juan': 25, 'Ana': 30, 'Pedro': 35}
-for nombre, edad in personas.items():
-    print(f"{nombre} tiene {edad} años")
+numeros = [3,7,2,9,5]
+buscar=7
+encontrado= False
+
+for numero in numeros:
+    if numero == buscar:
+        encontrado = True
+        break
+    
+if encontrado:
+    print (f"El número {buscar} está en la lista.")
+else:
+    print(f"El número {buscar} no está en la lista.")
+
+
