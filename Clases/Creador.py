c21eef655b27074a7cf786b8dcc17332c0f508d0
JesicaC266 <jesicacasallas06@gmail.com>
2025-01-27 17:59:19 -0500

    act creador

---
+++ Clases/Creador.py
@@ -0,0 +1,14 @@
+
+from clases.persona import Persona
+from clases.pelota import Pelota
+from clases.celular import celular
+from clases.vaso import vaso
+
+
+
+persona1 = Persona("Juan", 30)
+persona1.saludar()
+
+
+pelota1 = Pelota("Fútbol", "rojo")
+pelota1.rebotar()
