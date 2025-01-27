8455da51fa4a42cd844b29d9da71314a1493a7f1
JesicaC266 <jesicacasallas06@gmail.com>
2025-01-27 17:16:42 -0500

    act api

---
+++ API/URL.py
@@ -0,0 +1,3 @@
+url = 'https://www.stats.govt.nz/assets/Uploads/Alcohol-available-for-consumption/Alcohol-available-for-consumption-Year-ended-December-2023/Download-data/alcohol-available-for-consumption-year-ended-december-2023.csv'
+iris_df = pd.read_csv(url)
+iris_df
